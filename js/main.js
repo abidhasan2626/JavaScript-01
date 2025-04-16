e6b480052c0516ec675dcfd8607867b6404bfcb5
Abid Hasan <118461844+abidhasan2626@users.noreply.github.com>
2025-04-16 21:42:10 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Date and Time JavaScript Function
+
+function showDate() {
+	document.getElementById('date-time').innerHTML = Date();
+}
+
+// Text JavaScript Function
+function showHiddenText() {
+	document.getElementById('st').innerHTML = "Hello World !";
+}
+
+// Change Inner HTML Text JavaScript Function
+function changeText() {
+	document.getElementById('change-text').innerHTML = "I'm a Web Designer & Developer";
+}
+
